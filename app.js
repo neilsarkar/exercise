5fca1395a4b83af9e99aa899eafa6e483a0829fe
Neil Sarkar <neil@gotinder.com>
2015-10-28 22:34:01 -0700

    Add frontend component

--- app.js
+++ app.js
@@ -2,53 +2,81 @@ var async   = require('async');
 var mongodb = require('mongodb');
 var redis   = require('redis');
 var pg      = require('pg');
+var express = require('express');
+var app     = express();
 
-async.parallel({
-  mongo: function (cb) {
-    var url = 'mongodb://chicken:tetrazzini@halp:27019/porcelain';
-    mongodb.MongoClient.connect(url, function(err, db) {
-      if( err ) { return cb(err); }
 
-      db.close(cb);
-    })
-  },
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html', function(err) {
+    if( err ) {
+      console.error(err);
+      res.status(err.status).end();
+    }
+  })
+});
 
-  redis: function(cb) {
-    var url = 'redis://amber:energy@halp:6380';
-    var client = redis.createClient(url);
-    client.on('error', function(err) {
-      throw err;
-    })
+app.post('/message', createMessage);
 
-    client.set('cool', 'nice');
-    client.get('cool', function(err, reply) {
-      if( err ) { return cb(err); }
-      if( reply != 'nice' ) { return cb(new Error("Redis value was not set")); }
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-      client.quit();
-      cb();
-    })
-  },
+  console.log('Example app listening at http://%s:%s', host, port);
+});
 
-  postgres: function(cb) {
-    var url = 'postgres://outside:lookingin@halp/bluebell';
-
-    pg.connect(url, function(err, client, done) {
-      if( err ) { return cb(err); }
-
-      client.query("insert into hats (color, type, size) values ('blue', 'bowler', 3)", function(err, result) {
+function createMessage(req, res) {
+  async.parallel({
+    mongo: function (cb) {
+      var url = 'mongodb://chicken:tetrazzini@halp:27019/porcelain';
+      mongodb.MongoClient.connect(url, function(err, db) {
         if( err ) { return cb(err); }
-        client.query('select * from hats order by id desc limit 1', function(err, result) {
-          if( err ) { return cb(err); }
-          if( result.rows[0].color != 'blue' || result.rows[0].type != 'bowler' || result.rows[0].size != 3 ) { return cb(new Error("Postgres row not inserted"))}
-          client.end();
-          cb();
-        })
-      });
-    })
-  }
-}, function(err, results) {
-  if( err ) { throw err; }
 
-  console.log("Mmkay.");
-})
+        db.close(cb);
+      })
+    },
+
+    redis: function(cb) {
+      var url = 'redis://amber:energy@halp:6380';
+      var client = redis.createClient(url);
+      var failed = false;
+      client.on('error', function(err) {
+        if( failed ) { return; }
+        failed = true;
+        return cb(err);
+      })
+
+      client.set('cool', 'nice');
+      client.get('cool', function(err, reply) {
+        if( err && !failed ) { console.log("nope"); return cb(err); }
+        if( reply != 'nice' ) { return cb(new Error("Redis value was not set")); }
+
+        client.quit();
+        if( !failed ) {
+          cb();
+        }
+      })
+    },
+
+    postgres: function(cb) {
+      var url = 'postgres://outside:lookingin@halp/bluebell';
+
+      pg.connect(url, function(err, client, done) {
+        if( err ) { return cb(err); }
+
+        client.query("insert into hats (color, type, size) values ('blue', 'bowler', 3)", function(err, result) {
+          if( err ) { return cb(err); }
+          client.query('select * from hats order by id desc limit 1', function(err, result) {
+            if( err ) { return cb(err); }
+            if( result.rows[0].color != 'blue' || result.rows[0].type != 'bowler' || result.rows[0].size != 3 ) { return cb(new Error("Postgres row not inserted"))}
+            client.end();
+            cb();
+          })
+        });
+      })
+    }
+  }, function(err, results) {
+    if( err ) { return res.status(500).json({error: err}) }
+
+    res.send("Mmkay.");
+  })
+}
