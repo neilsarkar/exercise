1eb7fd06535b78b20f353a5b42334ae9a878188c
Neil Sarkar <neil@gotinder.com>
2015-10-28 21:38:19 -0700

    Add redis connection

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+var async = require('async');
 var mongodb = require('mongodb');
+var redis = require('redis');
 
-var url = 'mongodb://chicken:tetrazzini@halp:27019/porcelain';
+async.parallel({
+  mongo: function (cb) {
+    var url = 'mongodb://chicken:tetrazzini@halp:27019/porcelain';
+    mongodb.MongoClient.connect(url, function(err, db) {
+      if( err ) { return cb(err); }
 
-mongodb.MongoClient.connect(url, function(err, db) {
+      db.close(cb);
+    })
+  },
+
+  redis: function(cb) {
+    var url = 'redis://amber:energy@halp:6380';
+    var client = redis.createClient(url);
+    client.on('error', function(err) {
+      throw err;
+    })
+
+    client.set('cool', 'nice');
+    client.get('cool', function(err, reply) {
+      if( err ) { return cb(err); }
+      if( reply != 'nice' ) { return cb(new Error("Redis value was not set")); }
+
+      client.quit();
+      cb();
+    })
+  }
+}, function(err, results) {
   if( err ) { throw err; }
 
-  db.close(function(err, ok) {
-    if( err ) { throw err; }
-    console.log("Connected");
-  });
+  console.log("Mmkay.");
 })
